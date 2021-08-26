b21c35c434521acbd98d55e9f5751b67e7b2b260
SpiderWeb001 <govindrajput7508118778@gmail.comgit config --global user.email govindrajput7508118778@gmail.com>
2021-08-26 11:37:11 +0530

    add host

--- home/settings.py
+++ home/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bbqb+1g8)g(6-_mxt65ky$yd9a-dhi8kaylo+rgqi0#!lc5q30
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
